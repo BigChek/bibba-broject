2312a608017a321ff541802aca3b0a48ad53d030
BigChek <37079983+BigChek@users.noreply.github.com>
2018-03-05 12:33:07 -0500

    real stuff daqddie

---
+++ vidyaq/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace vidyaq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public abstract class People
+    {
+        public string name = "";
+        public int type = 0;
+        public abstract void attack()
+        { }
+    }
+    public class Character:People
+    {
+        public void Drdab()
+    {
+            Console.WriteLine("Dr.Dab");
+    }
+        public void Murphinator()
+        {
+            Console.WriteLine("The Murphinator");
+        }
+        public void Chef()
+        {
+            Console.WriteLine("Chef");
+        }
+    }
+    enum Type
+    {
+        Fire = 1, Normie, W0ke, Fighting, Water, Murph, Beaner, Nibba, 
+    }
+    
+    
+
+}
